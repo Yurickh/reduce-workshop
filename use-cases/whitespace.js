03362ac105899cf0954b2286d1bf90d10c44dc04
Yurick <yurick.hauschild@gmail.com>
2018-09-19 17:08:19 -0300

    ✨  Add whitespace use case

---
+++ use-cases/whitespace.js
@@ -0,0 +1,9 @@
+export default function whitespace(number) {
+  return 'nbsp;|'.repeat(number).split('|').reduce((acc, nbsp) => {
+    if (nbsp === '') {
+      return acc
+    }
+    acc.push(nbsp)
+    return acc
+  }, [])
+}
---
+++ use-cases/whitespace.test.js
@@ -0,0 +1,15 @@
+import whitespace from './whitespace'
+
+describe('whitespace', () => {
+  it('creates an array with length equal to the input', () => {
+    expect(whitespace(3)).toHaveLength(3)
+    expect(whitespace(5)).toHaveLength(5)
+    expect(whitespace(1)).toHaveLength(1)
+    expect(whitespace(0)).toHaveLength(0)
+    expect(whitespace(20)).toHaveLength(20)
+  })
+
+  it('fills the array with nbsp;', () => {
+    expect(whitespace(20).every(one => one === 'nbsp;')).toBe(true)
+  })
+})
